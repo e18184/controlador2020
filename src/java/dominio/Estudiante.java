33b3e5fbc1f98e775af0e742561523bf53e0e0fe
Richard Henry Sivila Rios <e18184@gmail.com>
2020-08-03 15:27:12 -0400

    03/08/2020 jstl estructuras

---
+++ src/java/dominio/Estudiante.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dominio;
+
+/**
+ *
+ * @author HAUSE
+ */
+public class Estudiante {
+    int ru;
+    String nombre;
+    String apellido;
+    String fechanacimiento;
+
+    public Estudiante(int ru, String nombre, String apellido, String fechanacimiento) {
+        this.ru = ru;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechanacimiento = fechanacimiento;
+    }
+
+    public int getRu() {
+        return ru;
+    }
+
+    public void setRu(int ru) {
+        this.ru = ru;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getFechanacimiento() {
+        return fechanacimiento;
+    }
+
+    public void setFechanacimiento(String fechanacimiento) {
+        this.fechanacimiento = fechanacimiento;
+    }
+    
+    
+}
