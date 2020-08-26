8e1a3c2a18c159aaf984fc517b920bf719800a58
Richard Henry Sivila Rios <e18184@gmail.com>
2020-08-26 14:03:52 -0400

    26/08/2020 jstl estructuras

--- src/java/controlador/ControladorController.java
+++ src/java/controlador/ControladorController.java
@@ -7,18 +7,46 @@ package controlador;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
+import dominio.Persona;
+import dominio.Estudiante;
+import org.springframework.stereotype.Controller;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 /**
  *
  * @author HAUSE
  */
-
+@Controller
+@RequestMapping("controlador.htm") 
 public class ControladorController {
- @RequestMapping("controlador.htm")  
+
+@RequestMapping(method=RequestMethod.GET)    
  public ModelAndView controlador(){
      // codigo
     ModelAndView modelo = new ModelAndView();
     modelo.setViewName("controlador");
-     return modelo;
+    Persona p =new Persona("Jose","mata",30);
+    modelo.addObject("persona",p);
+      return modelo;
  }
+ @RequestMapping(method=RequestMethod.POST)
+ public ModelAndView controladorPost(@ModelAttribute("persona") Persona persona){
+ // no es confiable <18  o >60
+ int edad=persona.getEdad();
+ if(edad<18 || edad>60){
+     System.out.println("Vacuna no confiable");
+ }else{
+     System.out.println("Vacuna confiable");
+ }
+     System.out.println("el ru es:"+persona.getNombre());
+     System.out.println("el lugar de nacimiiento es:"+persona.getLugarnacimiento());
+   ModelAndView modelo = new ModelAndView();
+  modelo.setViewName("controlador");
+  modelo.addObject("persona",persona);
+  return modelo;
+ }
+ 
 }
